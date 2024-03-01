56d153c8fa780324062be194654740cfe1ec03f7
Michael <111774557+MichaelKuch26@users.noreply.github.com>
2024-03-01 19:39:15 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const hwork = {
+  data() {
+    return {
+      articles: [
+        {
+          image: "./img/blogImg1.jpg",
+          imgText: "Kitchan Design",
+          itemTitle: "Let’s Get Solution For Building Construction\n Work",
+          itemData: "26 December,2022"
+        },
+        {
+          image: "./img/blogImg2.jpg",
+          imgText: "Living Design",
+          itemTitle: "Low Cost Latest Invented Interior Designing\n Ideas.",
+          itemData: "22 December,2022"
+        },
+        {
+          image: "./img/blogImg3.jpg",
+          imgText: "Interior Design",
+          itemTitle: "Best For Any Office & Business Interior\n Solution",
+          itemData: "25 December,2022"
+        },
+        {
+          image: "./img/blogImg4.jpg",
+          imgText: "Kitchan Design",
+          itemTitle: "Let’s Get Solution For Building  Construction\n Work",
+          itemData: "26 December,2022"
+        },
+        {
+          image: "./img/blogImg5.jpg",
+          imgText: "Living Design",
+          itemTitle: "Low Cost Latest Invented Interior Designing\n Ideas.",
+          itemData: "22 December,2022"
+        },
+        {
+          image: "./img/blogImg6.jpg",
+          imgText: "Interior Design",
+          itemTitle: "Best For Any Office & Business Interior\n Solution",
+          itemData: "25 December,2022"
+        },
+      ]
+    }
+  },
+}
+
+Vue.createApp(hwork).mount('#hwork'); 
